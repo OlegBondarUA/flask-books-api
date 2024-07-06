b158fb83ee90d94e5b1ef9ffe1b4e7469c356d86
OlegBondarUA <Petrovichteh@gmail.com>
2024-07-06 12:30:59 +0300

    Added CRUD operations REST API for managing books

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+from datetime import datetime
 
 
 app = Flask(__name__)
+# CORS(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///books.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
@@ -22,6 +25,7 @@ def create_tables():
         db.create_all()
 
 
+# GET /books: отримати список усіх книг
 @app.route('/books', methods=['GET'])
 def get_books():
     books = Book.query.all()
@@ -35,6 +39,58 @@ def get_books():
     } for book in books])
 
 
+# GET /books/<id>: Отримати деталі певної книги за ідентифікатором
+@app.route('/books/<int:id>', methods=['GET'])
+def get_book(id):
+    book = Book.query.get_or_404(id)
+    return jsonify({
+        "id": book.id,
+        "title": book.title,
+        "author": book.author,
+        "published_date": book.published_date.strftime('%Y-%m-%d'),
+        "isbn": book.isbn,
+        "pages": book.pages
+    })
+
+
+# POST /books: Додати нову книгу
+@app.route('/books', methods=['POST'])
+def add_book():
+    data = request.json
+    new_book = Book(
+        title=data['title'],
+        author=data['author'],
+        published_date=datetime.strptime(data['published_date'], '%Y-%m-%d').date(),
+        isbn=data['isbn'],
+        pages=data['pages']
+    )
+    db.session.add(new_book)
+    db.session.commit()
+    return jsonify({"message": "Book added successfully!", "id": new_book.id}), 201
+
+
+# PUT /books/<id>: оновити наявну книгу за ідентифікатором
+@app.route('/books/<int:id>', methods=['PUT'])
+def update_book(id):
+    book = Book.query.get_or_404(id)
+    data = request.json
+    book.title = data['title']
+    book.author = data['author']
+    book.published_date = datetime.strptime(data['published_date'], '%Y-%m-%d').date()
+    book.isbn = data['isbn']
+    book.pages = data['pages']
+    db.session.commit()
+    return jsonify({"message": "Book updated successfully!"})
+
+
+# DELETE /books/<id>: видалення книги за ідентифікатором
+@app.route('/books/<int:id>', methods=['DELETE'])
+def delete_book(id):
+    book = Book.query.get_or_404(id)
+    db.session.delete(book)
+    db.session.commit()
+    return jsonify({"message": "Book deleted successfully!"})
+
+
 if __name__ == '__main__':
-    create_tables()
     app.run(debug=True)
